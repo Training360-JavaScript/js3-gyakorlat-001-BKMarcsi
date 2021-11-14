1dc0e44d5e71368ba215c5f42910f0547818e3af
BKMarcsi <bkolonuszmaria@gmail.com>
2021-11-14 22:00:58 +0100

    js3-gyakorlat-001

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,6 @@
+const  removeDuplicatesFromArray = array => {
+    return array.filter( (element,index) => array.indexOf(element) === index);
+}
 
 
-
-
-
-
-// export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,19 @@
+let HU = {};
+
+let locale = 'hu-HU';
+
+HU.date = (date = new Date()) => {
+    return new Intl.DateTimeFormat(locale).format(date);
+}
+
+HU.curreny = (summa = 0) => {
+    return new Intl.NumberFormat(locale,{style:'currency',currency:'HUF'}).format(summa);
+}
+
+HU.list = (array = []) => {
+    return array.reduce((returnval,item,index) => {return (index < array.length-1?returnval+', '+item:returnval+' és '+item)});
+}
 
 
 
-
-
-
-// export default HU
+export default HU
